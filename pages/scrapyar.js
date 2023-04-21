bace417d088c8c69202719a5d03db3c7345d109a
tanmay2626~ <tanmaywaykar17@gmail.com>
2023-04-21 13:52:36 +0530

    Add Feeds Page for Scrapyar

--- pages/scrapyar.js
+++ pages/scrapyar.js
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "@/styles/Feed.module.scss";
 
 const Scrapyar = (props) => {
-  return <div>scrapyar</div>;
+  return (
+    <section className={styles.feeds_page}>
+      <p>Landing Page / Scrapyar</p>
+      <img src="/images/banner.png" alt="banner" />
+      <h1>Fresh recommendations</h1>
+      <section className={styles.feed}></section>
+    </section>
+  );
 };
 
 export default Scrapyar;
