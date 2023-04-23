47a55d8a960557b38027998185273454588ead1f
tanmay2626~ <tanmaywaykar17@gmail.com>
2023-04-23 12:50:40 +0530

    Update current site

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,21 @@ import styles from "@/styles/Home.module.scss";
 import { Divider } from "@mui/material";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleScrapyar = () => {
+    localStorage.setItem("current", "scrapyar");
+    router.push("/home/scrapyar");
+  };
+  const handleCreativeScrapyar = () => {
+    localStorage.setItem("current", "creative");
+    router.push("/home/creative-scrapyar");
+  };
+
   return (
     <>
       <main className={styles.main}>
@@ -16,15 +28,13 @@ export default function Home() {
           <div className={styles.heading}>
             <h2>Hi There!</h2>
             <h1>Start your journey with...</h1>
-            <Link href="/creative-scrapyar">
-              <button className={styles.right}>Creative ScrapYar</button>
-            </Link>
+            <button onClick={handleCreativeScrapyar} className={styles.right}>
+              Creative ScrapYar
+            </button>
             <Divider orientation="vertical" flexItem sx={{ fontSize: 15 }}>
               OR
             </Divider>
-            <Link href="/scrapyar">
-              <button>ScrapYar</button>
-            </Link>
+            <button onClick={handleScrapyar}>ScrapYar</button>
           </div>
         </section>
       </main>
