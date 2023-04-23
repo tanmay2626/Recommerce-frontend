9ddd9122a88fc534451e263bc41919d5178fe9cd
tanmay2626~ <tanmaywaykar17@gmail.com>
2023-04-23 21:56:35 +0530

    Add edit button profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import styles from "@/styles/UserProfile.module.scss";
-import { Divider } from "@mui/material";
+import { Button, Divider } from "@mui/material";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 const Profile = (props) => {
+  const router = useRouter();
+
   const [profile, setProfile] = useState(null);
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -20,15 +23,39 @@ const Profile = (props) => {
         console.log(error);
       });
   }, []);
+
+  const handleEditClick = () => {
+    router.push("/edit-profile");
+  };
   return (
     <section className={styles.profile}>
-      <img src="/images/profile.gif" alt="profile" />
+      <div className={styles.profile_header}>
+        <div>
+          <img src="/images/profile.gif" alt="profile" />
+        </div>
+        <div>
+          <h1>{profile?.name}</h1>
+          <Button
+            variant="contained"
+            size="small"
+            onClick={handleEditClick}
+            sx={{
+              color: "white",
+              fontSize: 1 + "rem",
+              textTransform: "none",
+              backgroundColor: "#088395",
+            }}
+          >
+            Edit
+          </Button>
+        </div>
+      </div>
       <div className={styles.details}>
         <h3>
-          @{profile.username} &#8226; {profile.email}
+          @{profile?.username} &#8226; {profile?.email}
         </h3>
       </div>
-      <div className={styles.address}>{profile.address}</div>
+      <div className={styles.address}>{profile?.address}</div>
       <h1>My views</h1>
       <Divider />
       <div className={styles.profile_section}>No Views yet</div>
