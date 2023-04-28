1077d4f72725cac05c5b31e867012ad1e90245da
Tanmay Waykar <87329832+tanmay2626@users.noreply.github.com>
2023-04-28 23:25:44 +0530

    Update index.js

--- pages/creative-scrapyar/index.js
+++ pages/creative-scrapyar/index.js
@@ -88,7 +88,7 @@ const CreativeScrapyar = (props) => {
     <>
       <Header />
       <section className={styles.feeds_page}>
-       <Link to="/"> <p>Landing Page / Scrapyar</p></Link>
+       <Link href="/"> <p>Landing Page / Scrapyar</p></Link>
         <img src="/images/Creative Banner.png" alt="banner" />
         <div className={styles.feed_headline}>
           <div>
